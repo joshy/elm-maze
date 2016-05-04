cbcd7387f2d7932b4433aec67436fa58351cc840
Joshy Cyriac <j.cyriac@gmail.com>
2016-05-04 20:56:45 +0200

    elm-format on cell.elm

--- cell.elm
+++ cell.elm
@@ -4,41 +4,62 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
-type Wall = N | E | S | W
+
+type Wall
+  = N
+  | E
+  | S
+  | W
+
 
 type alias Cell =
-  { walls: List Wall,
-    height: Float,
-    width: Float
+  { walls : List Wall
+  , height : Float
+  , width : Float
   }
 
 
 toForm : Cell -> Form
-toForm c  =
+toForm c =
   group <| List.map (wall red) (wallSegment c)
 
 
 wallSegment : Cell -> List Path
-wallSegment cell  =
+wallSegment cell =
   List.map (singleWall cell) cell.walls
+
 
 singleWall : Cell -> Wall -> Path
 singleWall cell w =
-  let hw = cell.width/2
-      hh = cell.height/2
+  let
+    hw =
+      cell.width / 2
+
+    hh =
+      cell.height / 2
   in
     case w of
-      N ->  segment (0-hw,hh) (hw,hh)
-      E ->  segment (hw,hh) (hw,0-hh)
-      S ->  segment (hw,0-hh) (0-hw,0-hh)
-      W ->  segment (0-hw,0-hh) (0-hw,hh)
+      N ->
+        segment ( 0 - hw, hh ) ( hw, hh )
+
+      E ->
+        segment ( hw, hh ) ( hw, 0 - hh )
+
+      S ->
+        segment ( hw, 0 - hh ) ( 0 - hw, 0 - hh )
+
+      W ->
+        segment ( 0 - hw, 0 - hh ) ( 0 - hw, hh )
 
 
 wall : Color -> Path -> Form
-wall color = traced (gridLine color)
+wall color =
+  traced (gridLine color)
 
 
 gridLine : Color -> LineStyle
 gridLine c =
   { defaultLine
-    | color = c, cap = Padded }
+    | color = c
+    , cap = Padded
+  }
